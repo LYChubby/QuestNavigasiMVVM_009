723bfedad3ee7cb6caa4560a1593085cee80e244
LYChubby <f4uzi345@gmail.com>
2024-11-21 16:43:50 +0700

    Membuat NavHost

---
+++ app/src/main/java/com/example/latihan5/ui/viewmodel/Navigation.kt
@@ -0,0 +1,34 @@
+package com.example.latihan5.ui.viewmodel
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
+enum class Halaman{
+    Form,
+    Data
+}
+
+@Composable
+fun Navigasi(
+    modifier: Modifier = Modifier,
+    viewModel: MahasiswaViewModel = viewModel(),
+    navHost: NavHostController = rememberNavController()
+) {
+    Scaffold { isipadding ->
+        val uiState by viewModel.dataModel.collectAsState()
+        NavHost(
+            modifier = modifier.padding(isipadding),
+            navController = navHost, startDestination = Halaman.Form.name
+        ) {
+
+        }
+    }
+}
