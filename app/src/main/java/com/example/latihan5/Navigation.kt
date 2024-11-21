b3901b3cc979b1f6f0560f6ed2cff665c9793232
LYChubby <f4uzi345@gmail.com>
2024-11-21 16:36:39 +0700

    Membuat fungsi Navigation dengan parameter

--- app/src/main/java/com/example/latihan5/Navigation.kt
+++ app/src/main/java/com/example/latihan5/Navigation.kt
@@ -1,6 +1,21 @@
 package com.example.latihan5
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.latihan5.ui.viewmodel.MahasiswaViewModel
+
 enum class Halaman{
     Form,
     Data
+}
+
+@Composable
+fun Navigation(
+    modifier: Modifier = Modifier,
+    viewModel: MahasiswaViewModel = viewModel(),
+    navHost: NavHostController = rememberNavController()
+) {
+
 }
