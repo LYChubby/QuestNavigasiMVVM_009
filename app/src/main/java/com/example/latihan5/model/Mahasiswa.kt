2a9928601f7e102b9dad0c968ad12b9981895b03
LYChubby <f4uzi345@gmail.com>
2024-11-21 16:22:21 +0700

    Menambahkan data NIM di data class Mahasiswa

--- app/src/main/java/com/example/latihan5/model/Mahasiswa.kt
+++ app/src/main/java/com/example/latihan5/model/Mahasiswa.kt
@@ -3,6 +3,7 @@ package com.example.latihan5.model
 data class Mahasiswa(
     val nama: String = "",
     val gender: String = "",
+    val nim: String = "",
     val alamat: String = "",
     val email: String = "",
     val nohp: String = "",
